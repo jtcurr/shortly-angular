c632b7057931730eea0489cdc1581d359077ec11
jtcurr <johntcurrie@gmail.com>
2017-01-20 20:16:03 -0800

    Pass all tests

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,11 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+////////////////////////////////////////////////////////////
+////Might need to pass a link into addLink to make it work//
+////////////////////////////////////////////////////////////
+  $scope.link = {};
+  $scope.addLink = function() {
+    Links.addOne();
+  };
 });
